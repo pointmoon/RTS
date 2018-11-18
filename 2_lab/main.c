5916425cf3b1f6ef0f362b345ff83ca2648d1643
pointmoon <dendy1996@yandex.ru>
2018-11-18 14:21:40 +0300

    twice comment

--- 2_lab/main.c
+++ 2_lab/main.c
@@ -1,6 +1,111 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <unistd.h>
 
-int main(){
-    printf("Hello World!\n");
-    return 0;
+#define RESET "\033[0m"
+#define YELLOW  "\033[1;33m"
+#define GREEN  "\033[1;32m"
+
+#define M 3 //Количество писателей
+#define N 3 //Количесвто читателей\
+
+unsigned int iter; //итерация
+sem_t accessM,readresM,orderM; //семафоры
+unsigned int readers = 0;	// Количество читателей, обращающихся к ресурсу
+
+void *reader(void *prm)
+{
+	int num1=*(int*)prm;
+	int i=0,r;
+	for(i;i<iter;i++)
+	{
+
+		if (sem_wait(&orderM)==0) printf("%s%d Читатель %d в очереди__________Ч%d%s\n",YELLOW,i,num1,num1,RESET);	// Помните наш порядок прибытия
+		sem_wait(&readresM);				 // Мы можем манипулировать счетчиками читателей
+		if (readers == 0)				// Если в настоящее время нет читателей (мы пришли первым)...
+			sem_wait(&accessM);				// ..запрашивает эксклюзивный доступ к ресурсу для читателей
+		readers++;							 // Обратите внимание, что есть еще один читатель
+		sem_post(&orderM);					 // Оформить заказ семафора прибытия (мы обслуживались)
+		sem_post(&readresM);				 // На данный момент мы получили доступ к числу читателей
+
+		printf("%s%d Работает читатель %d________________Ч%d%s\n",YELLOW,i,num1,num1,RESET);      // Здесь читатель может прочитать ресурс по своему усмотрению
+		r=1+rand()%4;
+		sleep(r);
+		sem_wait(&readresM);				 // Мы можем манипулировать счетчиками читателей
+		readers--;							 // Мы уходим, есть еще один читатель
+		if (readers == 0)				// Если в настоящее время читателей больше нет...
+			sem_post(&accessM);				// ...rосвободить эксклюзивный доступ к ресурсу
+		sem_post(&readresM);				 // На данный момент мы получили доступ к числу читателей
+	}
+}
+
+void *writer(void *prm)
+{
+	int num2=*(int*)prm;
+	int j=0,r;
+	for(j;j<iter;j++)
+	{
+		if(sem_wait(&orderM)==0) printf("%s%d Писатель %d в очереди__________П%d%s\n",GREEN,j,num2,num2,RESET); // Помните наш порядок прибытия
+		sem_wait(&accessM);					// Запросить эксклюзивный доступ к ресурсу
+		sem_post(&orderM);					 // Оформить заказ семафора прибытия (мы обслуживались)
+
+		printf("%s%d Работает писатель %d________________П%d%s\n",GREEN,j,num2,num2,RESET); // Здесь писатель может модифицировать ресурс по своему усмотрению
+		r=1+rand()%4;
+		sleep(r);
+		sem_post(&accessM);					// Отключить эксклюзивный доступ к ресурсу
+	}
+}
+
+void main()
+{
+	pthread_t threadRE[N];
+	pthread_t threadWR[M];
+	
+	if(sem_init(&accessM,0,1) != 0){
+		printf("ERROR (sem_init(&accessM,0,1))\n");
+		exit(EXIT_FAILURE);
+	}
+	if(sem_init(&readresM,0,1) != 0){
+		printf("ERROR sem_init(&readresM,0,1))\n");
+		sem_destroy(&accessM);
+		exit(EXIT_FAILURE);
+	}
+	if(sem_init(&orderM,0,1) != 0){
+		printf("ERROR sem_init(&orderM,0,1))\n");
+		sem_destroy(&accessM);
+		sem_destroy(&readresM);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Введите количество итераций: ");
+	scanf("%d",&iter);
+	printf("Iter                         ОЧЕРЕДЬ/ВЫПОЛНЕНИЕ\n");
+	int i;
+	for(i=0;i<M;i++)
+	{
+		pthread_create(&(threadWR[i]),NULL,writer,(void*)&i);
+		sleep(1);
+	}
+	for(i=0;i<N;i++)
+	{
+		pthread_create(&(threadRE[i]),NULL,reader,(void*)&i);
+		sleep(1);
+	}
+
+
+	for(i=0;i<N;i++)
+	{
+		pthread_join(threadRE[i],NULL);
+	}
+	for(i=0;i<M;i++)
+	{
+		pthread_join(threadWR[i],NULL);
+	}
+
+	sem_destroy(&accessM);
+	sem_destroy(&readresM);
+	sem_destroy(&orderM);
 }
